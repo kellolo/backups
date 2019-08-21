c53596fa287b1f6a23f0863c668ac20d2e08b968
Jared Kotleta <87kell@gmail.com>
2019-08-21 17:50:19 +0200

    Add files via upload

---
+++ 5/src/components/lol/lol.jsx
@@ -0,0 +1,18 @@
+import './lol.css';
+
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+export default class lol extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="lol"></div>
+    )
+  }
+}
